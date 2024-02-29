fb85c0c6953d51132928c1e628c111cc9c3f593e
Bilal Tariq <bilza2023@gmail.com>
2024-03-01 02:30:57 +0500

    0.6 user added

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 
 const mongoose = require('mongoose');
-const TCodeSchema = require('./TCodeSchema');
-const TCode = require("./TCode");
+const TCodeSchema = require('./tcode/TCodeSchema');
+const UserSchema = require('./user/UserSchema');
+const TCode = require("./tcode/TCode");
+const User = require("./user/User");
 let registered = false;
 const tcodeModels = [];
 
 function getTcode(tcode_name){
     
     for (let i = 0; i < tcodeModels.length; i++) {
-        
         const tcode_model = tcodeModels[i];
         if(tcode_model.key == tcode_name){
             return tcode_model.value;
@@ -17,10 +18,15 @@ function getTcode(tcode_name){
  return false;    
 }
 
+function getUser(){
+   return  getTcode("user");
+}
+
 function registerTcode(list=[]){
+    debugger;
     if(!registered) {registered = true;}
     else {return {ok:false,message : "already registered"}}
-
+    
     for (let i = 0; i < list.length; i++) {    
         
         const tcode_name = list[i];
@@ -36,5 +42,13 @@ function registerTcode(list=[]){
             value : tcode_mdl
         });
     }
+    //--push user
+    const mongoose_user_mdl =  mongoose.model("User", UserSchema);
+    const userObj = new User(mongoose_user_mdl);
+    tcodeModels.push ({
+        key : "user",
+        value : userObj
+    });
+//   console.log("tcodeModels",tcodeModels);  
 }
-module.exports = {getTcode,registerTcode};
+module.exports = {getTcode,registerTcode,getUser};
